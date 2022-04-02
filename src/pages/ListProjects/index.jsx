f2f452cdf94b61ed3a88cec4ff8583c322b7b187
Daniel Xavier <dxavier194@gmail.com>
2022-04-01 21:40:11 -0300

    Updated ListProjects

--- src/pages/ListProjects/index.jsx
+++ src/pages/ListProjects/index.jsx
@@ -1,37 +1,39 @@
 import React from 'react'
 import axios from 'axios'
 
-import  './styles.css';
+import './stylesListProjects.css';
 
 export default class ListProjects extends React.Component {
   state = {
-    tarefas: []
+    projetos: []
   }
 
   componentDidMount() {
     axios.get('https://immense-sands-97611.herokuapp.com/api/v1/projects')
       .then(res => {
-        const tarefas = res.data
+        const projetos = res.data
         console.log(res.data)
-        this.setState({ tarefas })
+        this.setState({ projetos })
       })
   }
 
   render() {
     return (
-        <>
+      <>
         <h1 align="center" className="margin">Lista dos Projetos</h1>
         <table className="table">
-            <tr className="thead-dark">
-                <td scope="col">Users</td>
-                <td scope="col">Name</td>
-                <td scope="col">Description</td>
-            </tr>
-            { this.state.tarefas.map(tarefa => 
-            <tr>    
-                <td  key={tarefa._id}>{tarefa.users.name}</td>
-                <td  key={tarefa._id}>{tarefa.name}</td>
-                <td  key={tarefa._id}>{tarefa.description}</td>
+          <tr className="thead-dark">
+            <th scope="col">Users</th>
+            <th scope="col">Function</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+          </tr>
+          {this.state.projetos.map(projeto =>
+            <tr className="bottom">
+              <td key={projeto._id}>{projeto.users[0].username}</td>
+              <td key={projeto._id}>{projeto.users[0].function}</td>
+              <td key={projeto._id}>{projeto.name}</td>
+              <td key={projeto._id}>{projeto.description}</td>
             </tr>)}
         </table>
 
