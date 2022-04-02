1b39f2b85502f67dd057833de13d6615efef6665
lucaslucenak <lucas.lucenak@gmail.com>
2022-04-01 21:16:48 -0300

    added about and authProvider

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => (
+    <div className="About">
+        <h1>About Page</h1>
+        <hr />
+        <p>Projeto desenvolvido para o trabalho de Tecnologias de Interface Gráficas. UEPB - Prof. Ramon</p>
+        <p>
+            Desenvolvidor por Daniel Xavier e Lucas de Lucena.
+        </p>
+    </div>
+);
+
+export default About;
